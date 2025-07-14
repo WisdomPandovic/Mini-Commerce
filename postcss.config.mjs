af7dedbab0d679ff803659f66e0fdce8ccfec86d
WisdomPandovic <wisdomokezieonwuchekwa@gmail.com>
2025-07-14 09:39:01 +0100

    fix: update postcss.config.js to resolve @tailwindcss/postcss and next/font errors

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,15 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+// postcss.config.js (or postcss.config.ts if you're using TS)
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
