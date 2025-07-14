74c29c1fb963581450b660d6cd4d5e2dbec29197
WisdomPandovic <wisdomokezieonwuchekwa@gmail.com>
2025-07-15 00:03:35 +0100

    fix: close mobile nav when cart icon is clicked

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -98,7 +98,7 @@ const Header: FC = () => {
               className="w-full border border-gray-300 px-3 py-1 rounded text-sm"
             />
           </form>
-          <Link href="/cart" className="text-sm relative w-fit self-start">
+          <Link href="/cart" className="text-sm relative w-fit self-start" onClick={toggleMenu}>
             <FaShoppingCart className="text-sm sm:text-base text-gray-700 hover:text-gray-900 cursor-pointer" />
             {items.length > 0 && (
               <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
